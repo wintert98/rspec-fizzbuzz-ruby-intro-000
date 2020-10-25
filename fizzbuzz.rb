1474e234c907d2da2fc1f52d1ef34bd6f1f1ebdc
Thomas Winter <wintert98@gmail.com>
2020-10-25 13:28:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(i)
+  
   if i % 3 == 0
     return "Fizz"
   elsif i % 5 == 0
     return "Buzz"
-  else i % 3 == 0 && i % 5 == 0
-    return "FizzBuzz"
+ 
   end
   
   
